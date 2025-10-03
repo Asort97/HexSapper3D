e6a97f4704e1720079a3d563cefbd17af5969290
Asort97 <Asort97@mail.ru>
2025-10-03 16:09:01 +0300

    Gamepush imported

---
+++ Assets/Plugins/GamePush/Data/ProjectData.cs
@@ -0,0 +1,13 @@
+namespace GamePush.Data
+{
+    public static class ProjectData
+    {
+        public static string SDK_VERSION = "1.8.1";
+        public static string ID = "24611";
+        public static string TOKEN = "JA8Y7Ii4S6zQb1kDEELcQOoPR1UJWDZf";
+        public static bool GAMEREADY_AUTOCALL = false;
+        public static bool SHOW_STICKY_ON_START = true;
+        public static bool WAIT_PLAGIN_READY = false;
+        public static bool AUTO_PAUSE_ON_ADS = false;
+    }
+}
---
+++ Assets/Plugins/GamePush/JS/_dataFields.jspre
@@ -0,0 +1,3 @@
+const dataProjectId = '24611';
+const dataPublicToken = 'JA8Y7Ii4S6zQb1kDEELcQOoPR1UJWDZf';
+const showPreloaderAd = 'True';
